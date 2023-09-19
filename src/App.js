9b2de073d9f772b98053d9a565903ceba1901dfe
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-09-19 20:25:30 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Bem-vindo ao Meu Aplicativo React</h1>
+          <p className="App-description">Esta é a minha tela inicial modificada.</p>
+        </header>
+      </div>
   );
 }
 
