82d406d4b9e1301ffcb8360714c2a42203be5201
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-09-19 21:16:17 -0300

    estruturando + tela inicial

---
+++ src/componentes/highbar.jsx
@@ -0,0 +1,15 @@
+import 'src/css/highbar.css';
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Bem-vindo ao Meu Aplicativo React</h1>
+                <p className="App-description">Esta é a minha tela inicial modificada.</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/componentes/telaInicial.jsx
@@ -0,0 +1,14 @@
+import 'src/css/highbar.css';
+
+
+function TelaInicial() {
+    return (
+        <div className="">
+            <header className="">
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
